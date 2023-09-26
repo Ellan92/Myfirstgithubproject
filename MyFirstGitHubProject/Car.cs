f862d88ab440daf834955f4dd1fce0ff09178480
Ellan92 <elle9121@hotmail.se>
2023-09-26 09:54:20 +0200

    Add car

---
+++ MyFirstGitHubProject/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace MyFirstGitHubProject
+{
+    internal class Car
+    {
+        int _maxSpeed = 250;
+        bool _engineStarted = false;
+        string _carBrand = "Jaguar";
+
+        public Car()
+        {
+
+        }
+
+    }
+}
