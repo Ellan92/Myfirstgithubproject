741df5735b0b7714ac8da34c8661168c38e70349
Ellan92 <elle9121@hotmail.se>
2023-09-26 10:22:20 +0200

    add ask for input

--- MyFirstGitHubProject/Program.cs
+++ MyFirstGitHubProject/Program.cs
@@ -4,13 +4,10 @@ Console.WriteLine("This is another line of code");
 
 Console.WriteLine("This is a third line of code!!!!!!!!!!!!!!!!!");
 
-string name = "Bosse";
 
-if (name == "Bosse")
-{
-    Console.WriteLine("Bosse!!!!!!!!!!");
-}
-else
-{
-    Console.WriteLine("No bosse :(");
-}
+//implementing feature ask for input
+
+Console.WriteLine("What's your name?");
+Console.Write("Name: ");
+
+string name = Console.ReadLine();
